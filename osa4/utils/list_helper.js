f3819a3c28dde5614936a068351b3988da0f56b1
Lofman <kimmo.lofman@gmail.com>
2019-07-04 18:31:03 +0300

    exercises from 4.3 to 4.7

---
+++ osa4/utils/list_helper.js
@@ -0,0 +1,77 @@
+const _ = require('lodash')
+
+
+const dummy = (blogs) => {
+  return 1
+}
+
+const totalLikes = (blogs) => {
+  const reducer = (sum, item) => {
+    return sum + item.likes
+  }
+  return blogs.length === 0
+      ? 0
+      : blogs.reduce(reducer, 0)
+
+}
+const favoriteBlog = (blogs) =>{
+
+  if(blogs.length === 0){
+    return {title: 'Empty list, no favorites'}
+  }
+
+  const max = blogs.reduce(function(prev, current) {
+    return (prev.likes > current.likes) ? prev : current
+  })
+
+  return {
+    title: max.title,
+    author: max.author,
+    likes: max.likes
+  }
+
+}
+const mostBlogs = (blogs) =>{
+
+  if(blogs.length === 0){
+    return {author: 'Empty list', blogs: 0}
+  }
+
+
+ let result = _(blogs)
+     .countBy('author')
+     .map((key, value) =>({
+       author: value,
+       blogs: key
+     }))
+     .maxBy('blogs')
+
+  console.log('Most Blogs', result)
+
+  return result
+}
+const mostLikes = (blogs) =>{
+
+  if(blogs.length === 0){
+    return {author: 'Empty list', likes: 0}
+  }
+
+  let result = _(blogs)
+      .groupBy('author')
+      .map((blog ,id)=>({
+        author: id,
+        likes: _.sumBy(blog, 'likes')
+      }))
+      .maxBy('likes')
+
+  console.log('Most likes', result)
+  return result
+}
+
+module.exports = {
+  dummy,
+  totalLikes,
+  favoriteBlog,
+  mostBlogs,
+  mostLikes
+}
