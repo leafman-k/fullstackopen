3add3366bc68b2cd205281d1016cc3237bcc6311
Lofman <kimmo.lofman@gmail.com>
2019-08-15 21:03:24 +0300

    Added exercises 5.3 and 5.4

--- osa5/bloglist-frontend/src/App.js
+++ osa5/bloglist-frontend/src/App.js
@@ -3,13 +3,17 @@ import Blog from './components/Blog'
 import loginService from './services/login'
 import blogService from './services/blogs'
 import Notification from './components/Notification'
+import BlogForm from './components/BlogForm'
 
 function App() {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
-  const [errorMessage, setErrorMessage] = useState(null)
+  const [message, setMessage] = useState(null)
   const [blogs, setBlogs] = useState([])
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] = useState('')
 
   useEffect(() => {
     blogService
@@ -24,6 +28,8 @@ function App() {
     if (loggedUserJSON) {
       const user = JSON.parse(loggedUserJSON)
       setUser(user)
+      blogService.setToken(user.token)
+
     }
   }, [])
 
@@ -60,28 +66,63 @@ function App() {
       window.localStorage.setItem(
           'loggedUser', JSON.stringify(user)
       )
+      blogService.setToken(user.token)
       setUser(user)
       setUsername('')
       setPassword('')
 
     } catch (exception) {
-      setErrorMessage('wrong credentials')
+      setMessage({text:'wrong credentials', type:'error'})
       setTimeout(() => {
-        setErrorMessage(null)
+        setMessage(null)
+      }, 5000)
+    }
+  }
+  const addBlog = async (event) => {
+    event.preventDefault()
+    try {
+      const addedBlog = await blogService.addBlog({
+        title, author, url
+      })
+      setBlogs(blogs.concat(addedBlog))
+      setMessage(
+          {text: `A new blog ${addedBlog.title} by ${addedBlog.author} added`, type: 'info'}
+      )
+      setTitle('')
+      setAuthor('')
+      setUrl('')
+
+    } catch (exception) {
+      setMessage({text:'Ooops, Something went wrong', type:'error'})
+      setTimeout(() => {
+        setMessage(null)
       }, 5000)
     }
   }
   const handleLogout = () => {
-
-
       window.localStorage.removeItem(
           'loggedUser'
       )
       setUser(null)
   }
+
+
+  const handleTitleChange = (event) => {
+    console.log(event.target.value)
+    setTitle(event.target.value)
+  }
+
+  const handleAuthorChange = (event) => {
+    console.log(event.target.value)
+    setAuthor(event.target.value)
+  }
+  const handleUrlChange = (event) => {
+    console.log(event.target.value)
+    setUrl(event.target.value)
+  }
   return (
     <div>
-      <Notification message={errorMessage}/>
+      <Notification message={message}/>
       {user === null ?
           loginForm() :
           <div>
@@ -90,6 +131,13 @@ function App() {
                 logout
               </button>
             </p>
+            <BlogForm addBlog={addBlog}
+                      title={title}
+                      author={author}
+                      url={url}
+                      handleTitleChange={handleTitleChange}
+                      handleAuthorChange={handleAuthorChange}
+                      handleUrlChange={handleUrlChange}/>
             <div>
               <h2>blogs</h2>
               {blogs.map(blog =>
