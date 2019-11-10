fe8b6723bc09c7087f92765d8e2666815ce46593
Lofman <kimmo.lofman@gmail.com>
2019-11-10 21:14:35 +0200

    Added exercises 8.24 -8.26

--- osa8/library-frontend/src/App.js
+++ osa8/library-frontend/src/App.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react'
+import React, {useState} from 'react'
 import {gql} from 'apollo-boost'
-import {useQuery, useMutation, useApolloClient} from '@apollo/react-hooks'
+import {useQuery, useMutation, useSubscription, useApolloClient} from '@apollo/react-hooks'
 import Authors from './components/Authors'
 import Books from './components/Books'
 import NewBook from './components/NewBook'
@@ -60,7 +60,17 @@ const LOGIN = gql`
     }
   }
 `
-
+const BOOK_ADDED = gql`
+subscription {
+  bookAdded {
+    title
+    author{
+      name
+    }
+    published
+  }
+}
+`
 const App = () => {
   const client = useApolloClient()
   const [page, setPage] = useState('authors')
@@ -69,10 +79,32 @@ const App = () => {
   const authors = useQuery(ALL_AUTHORS)
   const books = useQuery(ALL_BOOKS)
   const [addBook] = useMutation(CREATE_BOOK, {
-    refetchQueries: [{ query: ALL_BOOKS }, { query: ALL_AUTHORS }]
+    refetchQueries: [{query: ALL_BOOKS}, {query: ALL_AUTHORS}]
   })
   const [updateAuthor] = useMutation(EDIT_AGE, {
-    refetchQueries: [{ query: ALL_AUTHORS }]
+    refetchQueries: [{query: ALL_AUTHORS}]
+  })
+  const updateCacheWith = (addedBook) => {
+    const includedIn = (set, object) =>
+        set.map(p => p.id).includes(object.id)
+
+    const dataInStore = client.readQuery({query: ALL_BOOKS})
+    if (!includedIn(dataInStore.allBooks, addedBook)) {
+      client.writeQuery({
+        query: ALL_BOOKS,
+        data: {allBooks: dataInStore.allBooks.concat(addedBook)}
+      })
+    }
+  }
+  useSubscription(BOOK_ADDED, {
+    onSubscriptionData: ({subscriptionData}) => {
+      const addedBook = subscriptionData.data.bookAdded
+      setErrorMessage(`${addedBook.title} added`)
+      updateCacheWith(addedBook)
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 1500)
+    }
   })
   const handleError = (error) => {
     setErrorMessage(error.graphQLErrors[0].message)
@@ -92,32 +124,32 @@ const App = () => {
 
 
   return (
-    <div>
       <div>
-        <button onClick={() => setPage('authors')}>authors</button>
-        <button onClick={() => setPage('books')}>books</button>
-        {
-          token && <button onClick={() => setPage('add')}>add book</button>
+        <div>
+          <button onClick={() => setPage('authors')}>authors</button>
+          <button onClick={() => setPage('books')}>books</button>
+          {
+            token && <button onClick={() => setPage('add')}>add book</button>
+          }
+          {
+            token && <button onClick={() => setPage('recommendation')}>Recommended</button>
+          }
+          {
+            token && <button onClick={() => logout()}>logout</button>
+          }
+          {!token && <button onClick={() => setPage('login')}>login </button>}
+        </div>
+        {errorMessage &&
+        <div style={{color: 'red'}}>
+          {errorMessage}
+        </div>
         }
-        {
-          token && <button onClick={() => setPage('recommendation')}>Recommended</button>
-        }
-        {
-          token && <button onClick={() => logout()}>logout</button>
-        }
-        {!token && <button onClick={() => setPage('login')}>login </button>}
+        <Authors show={page === 'authors'} result={authors} editAuthor={updateAuthor}/>
+        <Books show={page === 'books'} result={books}/>
+        <NewBook show={page === 'add'} addBook={addBook}/>
+        <LoginForm show={page === 'login'} login={login} setToken={setToken}/>
+        <Recommendation show={page === 'recommendation'} result={books}/>
       </div>
-      {errorMessage &&
-      <div style={{color: 'red'}}>
-        {errorMessage}
-      </div>
-      }
-      <Authors show={page === 'authors'} result={authors} editAuthor={updateAuthor} />
-      <Books  show={page === 'books'} result={books} />
-      <NewBook show={page === 'add'} addBook={addBook}/>
-      <LoginForm show={page === 'login'} login={login} setToken={setToken}/>
-      <Recommendation show={page === 'recommendation'} result={books} />
-    </div>
   )
 }
 
